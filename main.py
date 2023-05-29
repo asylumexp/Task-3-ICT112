008a27cc5f8ccacb0c9f36ecbff1a2696a462365
Sam Heinz <sam@samheinz.com>
2023-05-29 20:20:08 +1000

    Replace with arrow selector

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from datastore import DataStore
 from ui import Ui
 
@@ -5,19 +7,24 @@ data_store = DataStore()
 ui = Ui()
 
 
-def main():
+def main(restart=False):
+    if restart:
+        ui.selection = 0
+
     user_input = ui.show_menu()
 
     match user_input:
-        case 1:
+        case 0:
             files = data_store.retrieve_local_rooms()
             chosen_file = ui.import_file(files)
-            if chosen_file == "":
-                main()
+            if chosen_file == -1:
+                main(True)
             else:
                 data_store.replace_data(chosen_file)
-        case 2:
+        case 1:
             data_store.data_import()
+        case 2:
+            sys.exit(-1)
 
 
 if __name__ == "__main__":
--- ui.py
+++ ui.py
@@ -1,38 +1,131 @@
 import sys
+import os
+if sys.platform == "win32":
+    import msvcrt
+else:
+    import termios
+    import tty
 
 
 class Ui:
-    @staticmethod
-    def show_menu():
-        print("""WELCOME""")
-        print("Retrieve from file (1) or load sample (2)")
+    def __init__(self):
+        self.selection = 0
+        self.welcome = """WELCOME"""
 
-        user_input = input().strip()
+    def show_menu(self):
+        self.get_menu_selection(["Import rooms from file", "Import sample rooms", "Quit"])
+        return self.selection
 
-        if user_input != "1" and user_input != "2":
-            sys.exit(255)
+    def import_file(self, files: list):
+        files.append("Return to main menu")
+        self.get_menu_selection(files)
+
+        if self.selection == len(files)-1:
+            return -1
         else:
-            return int(user_input)
+            return files[self.selection]
 
     def menu_selection(self):
         pass
 
-    @staticmethod
-    def import_file(files):
-        print(f"There was {len(files)} {'files' if len(files) > 1 else 'file'} found.")
-        i = 0
-        for file in files:
-            i += 1
-            print(f"    {i}. {file}")
-        try:
-            num = int(input("Which file would you like to import? (Type 0 to cancel)\n"))
-            if num == 0:
-                return ""
+    def get_menu_selection(self, menu):
+        """
+            Description:
+                Uses self.captureKeys to get the pressed key and make subsequent changes. If pressed key is enter 
+                then it returns to the main function so that the menu can change what is shown.
+        """
+
+        self.print_options(menu)
+        while True:
+            key = self.capture_keys(max_k=11)
+            if key == "up" or key == "down":
+                self.print_options(menu)
+            elif key == "enter":
+                return
+
+    def print_options(self, menu: list):
+        """
+            Description:
+                Prints all the main menu options,
+                changing colours and adding indicators for selected options.
+        """
+
+        self.clear_screen()
+        print('\x1b[1;130;44m', self.welcome, '\x1b[0m')
+
+        # * Print options on screen
+        for option in range(len(menu)):
+            # * Show indicator if current selected matches option
+            if self.selection == option:
+                print('\x1b[6;30;42m' + "<•>", menu[option], '\x1b[0m')
             else:
-                return files[num]
-        except ValueError:
-            print("Invalid value, returning to menu.")
-            return ""
-        except IndexError:
-            print("Invalid value, returning to menu.")
-            return ""
+                print("<○>", menu[option])
+
+    @staticmethod
+    def clear_screen():
+        """
+            Description:
+                Clears screen, with considerations for *nix and Windows operating systems different commands.
+        """
+        if os.name == 'posix':
+            os.system('clear')
+        else:
+            os.system('cls')
+
+    def capture_keys(self, max_k):
+        while True:
+            # * Capture key input
+            key = ord(self.getch())
+            # * Switch case for pressed keys
+            match key:
+                case 13:
+                    key = "enter"
+                    return key
+                case 72:
+                    if self.selection == 0:
+                        pass
+                    else:
+                        self.selection -= 1
+                        key = "up"
+                        return key
+                case 80:
+                    if self.selection == max_k:
+                        pass
+                    else:
+                        self.selection += 1
+                        key = "down"
+                        return key
+
+    @staticmethod
+    def getch(char_width=1):
+        """
+            Description:
+                Gets pressed key and returns it
+
+            Args:
+                char_width (int, optional): character width. Defaults to 1.
+
+            Returns:
+                pressed key (unicode character):Returns the pressed key as a unicode character
+        """
+
+        if sys.platform == "win32":
+            key = msvcrt.getch()
+            return key
+        else:
+            # ? Credit = "https://www.reddit.com/r/learnprogramming/comments/10wpkp/python_getch_in_unix/"
+            '''get a fixed number of typed characters from the terminal. 
+        Linux / Mac only'''
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setraw(fd)
+                ch = sys.stdin.read(char_width)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            # * NIX and Windows systems have different characters for the arrow keys, fixing them to match windows
+            if ord(ch) == 65:
+                ch = chr(72)
+            elif ord(ch) == 66:
+                ch = chr(80)
+            return ch
